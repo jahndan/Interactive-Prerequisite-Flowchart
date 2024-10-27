e70591daac9d441355f57f017a6c29b60eae4d64
jahndan <jahndandot@gmail.com>
2024-10-27 18:17:45 -0500

    cleaner setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // Development logging
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  // Production builds
+  compiler: {
+    // Remove console logs in production
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
 
 export default nextConfig;
